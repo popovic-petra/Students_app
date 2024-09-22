c4beb43b3a9df57a083f2a418b249e7b36d5fb5e
Petra Popovic <popovicpetra99@gmail.com>
2024-09-22 17:07:35 +0200

    Add two new students

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ def main():
     students = [Student("Mario", "Mandic", "1234"),
                 Student("Ana", "Ljubic", "2345"),
                 Student("Petra", "Popovic", "3432"),
+                Student("Mara", "Mandic", "3432"),
+                Student("Grgo", "Jovanic", "3432"),
                 Student("Ana", "Mikic", "3221")]
     
     courses = [Course("Programiranje 1", "S101"),
